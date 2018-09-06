e8ad2a8652359f08ff395bcb4155dbea6060bf9e
zl186 <zl186@duke.edu>
2018-09-05 23:33:59 -0400

    15_1

--- 015_code2/code2.c
+++ 015_code2/code2.c
@@ -1,33 +1,31 @@
 int printTriangle(int size) {
-  //start with starCount being 0
+  int starCount = 0;  //start with starCount being 0
 
-  //count from 0 (inclusive) to size (exclusive),
-  //for each number i that you count
+  for (int i = 0; i < size; i++) {  //count from 0 (inclusive) to size (exclusive),
+    for (int j = 0; j <= i; j++) {  //count from 0 (inclusive) to i (inclusive),
+      printf("*");                  //print a "*"
+      starCount++;
+    }  //increment starCount
+    printf("\n");
+  }
+  //when you finish counting on j,
 
-    //count from 0 (inclusive) to i (inclusive),
-    //for each number j that you count
+  //print a newline ("\n")
 
-      //print a "*"
+  //when you finish counting on i,
 
-      //increment starCount
-
-    //when you finish counting on j, 
-
-    //print a newline ("\n")
-
-  //when you finish counting on i, 
-
-  //your answer is starCount
-
+  return starCount;  //your answer is starCount
 }
-
 
 int main(void) {
   int numStars;
   printf("Here is a triangle with height 4\n");
   numStars = printTriangle(4);
   printf("That triangle had %d total stars\n", numStars);
-  //now print "Here is a triangle with height 7\n"
+  printf("Here is a triangle with height 7\n");
+  numStars = printTriangle(7);
+  printf("That triangle had %d total stars\n",
+         numStars);  //now print "Here is a triangle with height 7\n"
 
   //then call printTriangle, passing in 7, and assign the result to
   //numStars
@@ -37,6 +35,3 @@ int main(void) {
 
   return 0;
 }
-
-
-
