4eafe374767f7cfbe04c583cd71ac08724baeaaf
zl186 <zl186@duke.edu>
2018-09-13 11:42:53 -0400

    027_1

---
+++ 027_tests_power/power.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned power(unsigned x, unsigned n) {
+  if (0 == n)
+    return 1;
+  else if (1 == n)
+    return x;
+  else
+    return x * power(x, n - 1);
+}
