7d1122cc0da3ed8a43877283f5ab703496e93c32
zl186 <zl186@duke.edu>
2018-10-20 16:56:33 -0400

    076

--- 076_read_templ2/code.cpp
+++ 076_read_templ2/code.cpp
@@ -18,8 +18,8 @@ std::ostream & operator<<(std::ostream & s, const std::vector<T> & v) {
 int main(void) {
   std::vector<int> v1;
   std::vector<std::vector<int> > v2;
-  for (size_t i =0; i < 4; i++) {
-    v1.push_back(2*i+3);
+  for (size_t i = 0; i < 4; i++) {
+    v1.push_back(2 * i + 3);
     v2.push_back(v1);
     std::cout << v1 << std::endl;
   }
