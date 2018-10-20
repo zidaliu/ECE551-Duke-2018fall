0e5a07d08c49756803e6870f14e02aaf6081db6f
zl186 <zl186@duke.edu>
2018-10-20 16:47:57 -0400

    075

--- 075_read_template/code.cpp
+++ 075_read_template/code.cpp
@@ -1,27 +1,30 @@
 #include <cstdlib>
 #include <iostream>
-template<typename T> class MyClass;
-template<typename T> std::ostream & operator<<(std::ostream & s, const MyClass<T> & x);
+template<typename T>
+class MyClass;
+template<typename T>
+std::ostream & operator<<(std::ostream & s, const MyClass<T> & x);
 
 template<typename T>
-class MyClass {
-private:
+class MyClass
+{
+ private:
   T data;
-public:
-  void setData(const T& d) {
-    data = d;
-  }
-  template<typename A> T f(const A& x) {
+
+ public:
+  void setData(const T & d) { data = d; }
+  template<typename A>
+  T f(const A & x) {
     return data + x;
   }
-  template<typename X> friend std::ostream & operator<<(std::ostream &s, const MyClass<X> & x);
+  template<typename X>
+  friend std::ostream & operator<<(std::ostream & s, const MyClass<X> & x);
 };
 
 template<typename T>
 std::ostream & operator<<(std::ostream & s, const MyClass<T> & x) {
   return s << x.data;
 }
-
 
 int main(void) {
   MyClass<std::string> s;
@@ -33,13 +36,13 @@ int main(void) {
 
   s.setData("Hello");
 
-  std::cout <<  s.f<std::string>(" World") << std::endl;
+  std::cout << s.f<std::string>(" World") << std::endl;
 
   std::cout << num.f<double>(3.14) << std::endl;
 
   MyClass<MyClass<int> > mc;
   mc.setData(num);
-  
+
   std::cout << mc << std::endl;
 
   return EXIT_SUCCESS;
