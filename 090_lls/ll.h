c5329d181a3040e6cd98790100db88da78da3b0a
zl186 <zl186@duke.edu>
2018-11-01 22:14:14 -0400

    0903

--- 090_lls/ll.h
+++ 090_lls/ll.h
@@ -21,7 +21,7 @@ class LinkedList
 
   class IndexError : public std::exception
   {
-    const char * what() { return "Index out of boundd"; }
+    const char * what() { return "out of bound"; }
   };
 
   Node * head;
@@ -152,67 +152,4 @@ int LinkedList<T>::find(const T & item) {
   return i;
 }
 
-/*
-void testList(void) {
-  IntList a;
-  if (a.getSize() != 0) {
-    exit(EXIT_FAILURE);
-  }
-
-  a.addFront(2);
-  a.addFront(3);
-  a.addBack(4);
-  IntList b(a);   //copy construct
-  IntList c = a;  //assignment operator
-
-  //TEST CASE original test case
-  if (a.head->data != 3 || a.head->next->data != 2 || a.getSize() != 3 ||
-      a.head->next->next->next != NULL || a.head->prev != NULL || a.tail->prev->data != 2 ||
-      a.tail->data != 4) {
-    exit(EXIT_FAILURE);
-  }
-  //copy construct test case
-  if (b.head->data != 3 || b.head->next->data != 2 || b.getSize() != 3 ||
-      b.head->next->next->next != NULL || b.head->prev != NULL || b.tail->prev->data != 2 ||
-      b.tail->data != 4) {
-    exit(EXIT_FAILURE);
-  }
-  //assignment operator test case
-  if (c.head->data != 3 || c.head->next->data != 2 || c.getSize() != 3 ||
-      c.head->next->next->next != NULL || c.head->prev != NULL || c.tail->prev->data != 2 ||
-      c.tail->data != 4) {
-    exit(EXIT_FAILURE);
-  }
-
-  //check a after mid remove
-  a.remove(3);
-  a.remove(2);
-  if (a.getSize() != 1 || a.head->data != 4 || a.tail->data != 4 || a.head->next != NULL ||
-      a.head->prev != NULL) {
-    exit(EXIT_FAILURE);
-  }
-
-  if (b.head->data != 3 || b.head->next->data != 2 || b.getSize() != 3 ||
-      b.head->next->next->next != NULL || b.head->prev != NULL || b.tail->prev->data != 2 ||
-      b.tail->data != 4) {
-    exit(EXIT_FAILURE);
-  }
-  if (c.head->data != 3 || c.head->next->data != 2 || c.getSize() != 3 ||
-      c.head->next->next->next != NULL || c.head->prev != NULL || c.tail->prev->data != 2 ||
-      c.tail->data != 4) {
-    exit(EXIT_FAILURE);
-  }
-  //re assignment c
-  c = a;
-  if (c.getSize() != 1 || c.head->data != 4 || c.tail->data != 4 || c.head->next != NULL ||
-      c.head->prev != NULL) {
-    exit(EXIT_FAILURE);
-  }
-  //REMOVE LAST element
-  c.remove(4);
-  if (c.getSize() != 0 || c.head != NULL || c.tail != NULL) {
-    exit(EXIT_FAILURE);
-  }
-}
-*/
 #endif
