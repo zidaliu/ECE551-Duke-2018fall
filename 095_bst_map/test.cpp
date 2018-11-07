4bec69136d938544766c87f9816181784ff5f072
zl186 <zl186@duke.edu>
2018-11-07 02:45:08 -0500

    095_4

--- 095_bst_map/test.cpp
+++ 095_bst_map/test.cpp
@@ -4,50 +4,53 @@
 
 #include "bstmap.h"
 
-class TestBstMap : public BstMap<std::string, int>
-{
-  void dumpNode(const Node * n, std::ostream & os) const {
-    os << '(';
-    if (n != NULL) {
-      os << n->key << ':' << n->value;
-      dumpNode(n->left, os);
-      dumpNode(n->right, os);
-    }
-    os << ')';
-  }
-
- public:
-  friend std::ostream & operator<<(std::ostream &, const TestBstMap &);
-};
-
-std::ostream & operator<<(std::ostream & os, const TestBstMap & m) {
-  m.dumpNode(m.root, os);
-  return os;
-}
-
 int main() {
-  TestBstMap m;
-  static int seq[] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2, 3, 8, 4, 6, 2,
-                      6, 4, 3, 3, 8, 3, 2, 7, 9, 5, 0, 2, 8, 8, 4, 1, 9, 7, 1, 6, 9, -1};
-  int has[10] = {0};
-  int n;
-  for (int i = 0; (n = seq[i]) != -1; i++) {
-    std::stringstream ss;
-    ss << n;
-    if (has[n]) {
-      m.remove(ss.str());
-    }
-    else {
-      m.add(ss.str(), i);
-    }
-    has[n] = !has[n];
-    std::cout << m << std::endl;
-  }
+  BstMap<char, int> m;
+  m.add('a', 10);
+  m.add('b', 5);
+  m.add('c', 15);
+  m.add('d', 20);
+  m.add('e', 7);
+  m.add('f', 21);
+  m.lookup('d');
   try {
-    std::cout << m.lookup("guess what?");
+    m.lookup('m');
   }
   catch (const std::invalid_argument & e) {
-    std::cout << "yeah, " << e.what() << "...\n";
+    std::cout << e.what() << "\n";
+  }
+  m.add('m', 23);
+  try {
+    m.lookup('m');
+  }
+  catch (const std::invalid_argument & e) {
+    std::cout << "Not add successful" << e.what() << "\n";
+  }
+  m.remove('m');
+  try {
+    m.lookup('m');
+  }
+  catch (const std::invalid_argument & e) {
+    std::cout << "Now remove sucess " << e.what() << "\n";
+  }
+  /*copy construct*/
+  BstMap<char, int> m_1(m);
+  try {
+    m_1.lookup('a');
+  }
+  catch (const std::invalid_argument & e) {
+    std::cout << "Not copy sucessful"
+              << "\n";
+  }
+
+  /*assignment operater*/
+  BstMap<char, int> m_2 = m;
+  try {
+    m_2.lookup('a');
+  }
+  catch (const std::invalid_argument & e) {
+    std::cout << "Not assign sucessful"
+              << "\n";
   }
 
   return 0;
