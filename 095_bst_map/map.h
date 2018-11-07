439612f03cc32e9da6e1c60eb111e926e7e8cee5
zl186 <zl186@duke.edu>
2018-11-07 01:56:43 -0500

    095_3

--- 095_bst_map/map.h
+++ 095_bst_map/map.h
@@ -3,11 +3,12 @@
 #include <stdexcept>
 
 template<typename K, typename V>
-class Map {
-public:
+class Map
+{
+ public:
   virtual void add(const K & key, const V & value) = 0;
-  virtual const V & lookup(const K& key) const throw (std::invalid_argument) = 0;
-  virtual void remove(const K& key) = 0;
-  virtual ~Map<K,V>() {}
+  virtual const V & lookup(const K & key) = 0;
+  virtual void remove(const K & key) = 0;
+  virtual ~Map<K, V>() {}
 };
 #endif
