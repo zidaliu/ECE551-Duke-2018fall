4289b333f0f56e4c4fdb626ed8f90fa33a9f4bce
zl186 <zl186@duke.edu>
2018-11-19 11:29:15 -0500

    103

--- 103_huff_all/compress_1.cpp
+++ 103_huff_all/compress_1.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <fstream>
 #include <functional>
 #include <iostream>
 #include <map>
@@ -10,6 +11,7 @@
 
 #include "node.h"
 #include "readFreq.h"
+using namespace std;
 
 void writeHeader(BitFileWriter * bfw, const std::map<unsigned, BitString> & theMap) {
   for (int i = 0; i < 257; i++) {
@@ -32,34 +34,21 @@ void writeCompressedOutput(const char * inFile,
 
   //WRITE YOUR CODE HERE!
   //open the input file for reading
-
-  FILE * f = fopen(inFile, "r");
-  if (f == NULL) {
-    perror("Could not open file");
-    exit(EXIT_FAILURE);
-  }
+  ifstream f(inFile);
+  char ch;
   //You need to read the input file, lookup the characters in the map,
-  //and write the proper bit string with the BitFileWriter
-  /*uint64_t * freq = readFrequencies(inFile);
-  for (unsigned i = 0; i < 257; i++) {
-    if (freq[i] == 0) {
-      continue;
-    }
-    BitString value = theMap.find(i)->second;
-    bfw.writeBitString(value);
-    }*/
-  int c;
-  while ((c = fgetc(f)) != EOF) {
-    BitString value = theMap.find(c)->second;
-    bfw.writeBitString(value);
-    //theMap.erase(c);
+  map<unsigned, BitString>::const_iterator iter;
+  while (f.get(ch)) {
+    iter = theMap.find((unsigned char)ch);
+    bfw.writeBitString(iter->second);
   }
+  //and write the proper bit string with the BitFileWriter
+  iter = theMap.find(256);
+  bfw.writeBitString(iter->second);
   //dont forget to lookup 256 for the EOF marker, and write it out.
-  BitString value = theMap.find(256)->second;
-  bfw.writeBitString(value);
+
   //BitFileWriter will close the output file in its destructor
   //but you probably need to close your input file.
-  fclose(f);
 }
 
 int main(int argc, char ** argv) {
@@ -71,14 +60,15 @@ int main(int argc, char ** argv) {
   //Implement main
   //hint 1: most of the work is already done.
   //hint 2: you can look at the main from the previous tester for 90% of this
-  uint64_t * counts = readFrequencies(argv[1]);
-  assert(counts != NULL);
-  Node * tree = buildTree(counts);
-  delete[] counts;
-  std::map<unsigned, BitString> theMap;
-  BitString empty;
-  tree->buildMap(empty, theMap);
-  writeCompressedOutput(argv[1], argv[2], theMap);
+  const char * input = argv[1];
+  const char * output = argv[2];
+  uint64_t * fre = readFrequencies(input);
+  Node * tree = buildTree(fre);
+  delete[] fre;
+  BitString b;
+  map<unsigned, BitString> theMap;
+  tree->buildMap(b, theMap);
   delete tree;
+  writeCompressedOutput(input, output, theMap);
   return EXIT_SUCCESS;
 }
